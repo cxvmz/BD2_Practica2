08d02d4b4382253eb23e5902b6d0a9edeba00f3b
ManuelMiranda99 <3057476220301@ingenieria.usac.edu.gt>
2021-09-21 23:27:24 -0600

    Se agrego consultas, aun por probar gg

--- [BD2] Practica2_DDL.sql
+++ [BD2] Practica2_DDL.sql
@@ -41,3 +41,65 @@ CREATE TABLE Detalle(
     FOREIGN KEY(id_factura) REFERENCE Factura(id_factura),
     FOREIGN KEY(id_producto) REFERENCE Producto(id_producto)
 );
+
+/*
+    TOP 10 DE LOS VENDEDORES QUE HAN CONCRETADO MÁS VENTAS
+*/
+SELECT
+    nombre_vendedor,
+    COUNT(Factura.id_vendedor) AS Ventas
+FROM Vendedor
+    INNER JOIN Factura
+        ON Factura.id_vendedor = Vendedor.id_vendedor
+GROUP BY Vendedor.id_vendedor
+ORDER BY Ventas DESC
+WHERE ROWNUM <= 10;
+
+/*
+    TOP 3 DE PRODUCTOS MAS VENDIDOS EN EL AÑO 2020
+*/
+SELECT
+    nombre_producto,
+    SUM(cantidad) AS Veces_Vendidas
+FROM Producto
+    INNER JOIN Detalle
+        ON Detalle.id_producto = Producto.id_producto
+    INNER JOIN Factura
+        ON Factura.id_factura = Detalle.id_factura
+GROUP BY Producto.id_producto
+ORDER BY Veces_Vendidas DESC
+WHERE
+    ROWNUM <= 3 AND
+    Factura.fecha_factura >= TO_DATE("01/JAN/2020", "dd/mon/yyyy") AND
+    Factura.fecha_factura <= TO_DATE("31/DEC/2020", "dd/mon/yyyy");
+
+/*
+    TOP 5 DE LOS PRODUCTOS QUE MENOS SE HAN VENDIDO EN EL AÑO 2021
+*/
+SELECT
+    nombre_producto,
+    SUM(cantidad) AS Veces_Vendidas
+FROM Producto
+    INNER JOIN Detalle
+        ON Detalle.id_producto = Producto.id_producto
+    INNER JOIN Factura
+        ON Factura.id_factura = Detalle.id_factura
+GROUP BY Producto.id_producto
+ORDER BY Veces_Vendidas ASC
+WHERE
+    ROWNUM <= 5 AND
+    Factura.fecha_factura >= TO_DATE("01/JAN/2021", "dd/mon/yyyy") AND
+    Factura.fecha_factura <= TO_DATE("31/DEC/2021", "dd/mon/yyyy");
+
+/*
+    TOP 5 DE VENDEDORES QUE HAN ATENDIDO UNA MAYOR CANTIDAD DE CLIENTES
+*/
+SELECT
+    nombre_vendedor,
+    COUNT(id_cliente) AS Clientes
+FROM Vendedor
+    INNER JOIN Factura
+        ON Factura.id_vendedor = Vendedor.id_vendedor
+GROUP BY nombre_vendedor, id_cliente
+ORDER BY Clientes DESC
+WHERE ROWNUM <= 5;
